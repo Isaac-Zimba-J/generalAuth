971803e3ea88e595f086242fe2ae401b4e35647f
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2024-11-20 12:42:56 +0200

    added other files

--- Shared/Services/Contracts/IUserService.cs
+++ Shared/Services/Contracts/IUserService.cs
@@ -5,12 +5,15 @@ namespace Shared.Services;
 public interface IUserService 
 {
     //register
-    Task<IdentityResult> RegisterAsync(ApplicationUser email, string password);
+    Task<IdentityResult> RegisterAsync(ApplicationUser user);
     //login
     Task<SignInResult> LoginAsync(string email, string password);
     //update user
     Task<IdentityResult> UpdateUserAsync(ApplicationUser user);
     //delete user
     Task<IdentityResult> DeleteUserAsync(string userId);
+    
+    // get all users
+    Task<List<ApplicationUser>> GetAllUsersAsync();
     
 }
